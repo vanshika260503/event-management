424a15f8aaebab78cf5813fc9a0d5b3b9d23d637
RadhikaChhabra17 <77332750+RadhikaChhabra17@users.noreply.github.com>
2021-08-06 00:51:32 +0530

    Add files via upload

---
+++ main/Main.java
@@ -0,0 +1,13 @@
+
+package main;
+import login.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		//AdminLogin.adminLogin();
+		//CustomerRegister.registerPrint();
+		CustomerLogin.customerLogin();
+	}
+
+}
