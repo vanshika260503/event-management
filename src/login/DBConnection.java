f5b684d37f342a7cd717b35aeca1bbc3ed68cc42
RadhikaChhabra17 <77332750+RadhikaChhabra17@users.noreply.github.com>
2021-08-05 23:47:49 +0530

    Update DBConnection.java

--- src/login/DBConnection.java
+++ src/login/DBConnection.java
@@ -15,8 +15,10 @@ public class DBConnection {
     	Class.forName("com.mysql.jdbc.Driver");	
 		 connect = DriverManager.getConnection(MySQL_CONN, MySQL_USERNAME , MySQL_PASSWORD);
 		
-		}catch(SQLException e) {
-			e.printStackTrace();		}
+		}
+	catch(SQLException e) {
+		e.printStackTrace();
+	}
     	return connect;
   }
 }
