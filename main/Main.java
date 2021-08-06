751bbb8bcd55a81d8b9b539fe76d8c475aaa3cf0
RadhikaChhabra17 <77332750+RadhikaChhabra17@users.noreply.github.com>
2021-08-06 10:33:13 +0530

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -6,9 +6,7 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		Welcome.start();
-		//AdminLogin.adminLogin();
-		//CustomerRegister.registerPrint();
-		//CustomerLogin.customerLogin();
+		
 	}
 
 }
