534fb39cc0d2cd8d9d8d96349fca1d825667b579
RadhikaChhabra17 <77332750+RadhikaChhabra17@users.noreply.github.com>
2021-08-07 19:01:22 +0530

    Update input.java

--- customer/input.java
+++ customer/input.java
@@ -1,26 +1,25 @@
 package customer;
 
-import java.sql.Date;
 import java.util.Scanner;
 
-public class input {
-	private static String date;
-	private static String place;
-	private static int people;
-	private static String eventType;
-    public static void input() {
+public class Input {
+	
+    public static Dashboard getInput() {
+	  Scanner input2 = new Scanner(System.in);
 	  Scanner input = new Scanner(System.in);
 	  System.out.print("Enter date of your event(dd/mm/yy) - ");
-	  date = input.nextLine();
+	  String date = input2.nextLine();
 	  System.out.print("Enter city of your event - ");
-	  place = input.nextLine();
+	  String place = input2.nextLine();
 	  System.out.print("Enter number of guests of your event - ");
-	  people = input.nextInt();
+	  int people = input2.nextInt();
 	  System.out.println("Enter the type of event - ");
-	  eventType = input.nextLine();
+	  String eventType = input.nextLine();
 	 
 	  Dashboard d = new Dashboard(date, place, people, eventType );	
-	  
+	  return d;
+	
   }
-    
+	
 }
+
