dfe4a158b84c76fa01317132398bc0b12754fe32
RadhikaChhabra17 <77332750+RadhikaChhabra17@users.noreply.github.com>
2021-08-07 23:24:27 +0530

    Update Details.java

--- customer/Details.java
+++ customer/Details.java
@@ -1,12 +1,7 @@
 package customer;
-import login.*;
-import static main.Main.d;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
 
 public abstract class Details extends Dashboard {
 	
@@ -16,39 +11,8 @@ public abstract class Details extends Dashboard {
 	
 	public  abstract  void ask() throws ClassNotFoundException, SQLException ;
 	
-	public  void getOption(String type, String tableName) throws ClassNotFoundException, SQLException {
-		Connection conn = DBConnection.connect();
-		Statement st = conn.createStatement();
-		ResultSet rs = st.executeQuery("SELECT id,name,type,city FROM " + tableName );
-		while(rs.next()) {
-			int id = rs.getInt("id");
-			String showname = rs.getString("name");
-			String showtype = rs.getString("type");
-			String city = rs.getString("city");
-			if(showtype.equals(type) && city.equals(((Dashboard) d).getPlace())) { 
-				System.out.println(showname + ":" + id);
-			}
-		
-		}
-	}
+	public abstract void getOption(String type, String tableName) throws ClassNotFoundException, SQLException;
 	
-	public  void getDetails(int id,String tableName) throws ClassNotFoundException, SQLException {
-		Connection conn = DBConnection.connect();
-		Statement st = conn.createStatement();
-		ResultSet rs = st.executeQuery("SELECT * FROM " + tableName);
-		while(rs.next()) {
-			int ID = rs.getInt("id");
-			String city = rs.getString("city");
-			String showname = rs.getString("name");
-			String type = rs.getString("type");
-			String number = rs.getString("phone");
-			if(ID == id) {
-				System.out.println("Name : " + showname +
-						"\ntype : " + type +
-						"\nCity : " + city +
-						"\nPhone Number : " + number );
-				break;
-			}
-		}
-	}
+	public abstract void getDetails(int id,String tableName) throws ClassNotFoundException, SQLException;
+	
 }
