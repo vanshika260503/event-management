8c040b0ba769ca78ac37ceceffd64058203396ee
RadhikaChhabra17 <77332750+RadhikaChhabra17@users.noreply.github.com>
2021-08-12 11:57:18 +0530

    Add files via upload

---
+++ login/DBConnection.java
@@ -0,0 +1,24 @@
+package login;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	static Connection connect;
+	private static final String MySQL_USERNAME = "root";
+    private static final String MySQL_PASSWORD = "Ra@170503";
+    private static final String MySQL_CONN = "jdbc:mysql://localhost:3306/event_managment";
+    
+    public static Connection connect() throws ClassNotFoundException  {
+    	try{
+    	Class.forName("com.mysql.jdbc.Driver");	
+		connect = DriverManager.getConnection(MySQL_CONN, MySQL_USERNAME , MySQL_PASSWORD);
+		
+		}
+    	catch(SQLException e) {
+			e.printStackTrace();
+		}
+    	return connect;
+  }
+}
