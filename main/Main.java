fd1bcc88388bc6cd089b290b5bba703485eaab50
RadhikaChhabra17 <77332750+RadhikaChhabra17@users.noreply.github.com>
2021-08-19 16:45:09 +0530

    Add files via upload

---
+++ main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+import customer.*;
+
+
+public class Main {
+    
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Welcome.start();
+		
+		//BookEvent.book();
+	}
+	}
