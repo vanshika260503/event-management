d7e8ab6cd8de0dbfa48cca5ae1379c0fb6ca8dc4
RadhikaChhabra17 <77332750+RadhikaChhabra17@users.noreply.github.com>
2021-08-19 16:43:38 +0530

    Add files via upload

---
+++ admin/AdminDashboard.java
@@ -0,0 +1,43 @@
+package admin;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class AdminDashboard {
+    int choice1,choice2;
+    Scanner input = new Scanner(System.in);
+
+    public  void Mainmenu() throws ClassNotFoundException, SQLException {
+        System.out.println("        WELCOME Admin Start your Day");
+        System.out.println("""
+                Choose an option:
+                Enter [1] to To know Customer bookings
+                Enter [2] to Update or view options in Inventory
+                Enter [0] to EXIT""");
+        choice1 = input.nextInt();
+        switch(choice1){
+            case 1 : var customer =new CustomerDetails();
+                      customer.display();
+            break;
+            case 2  : System.out.println(""" 
+                    Choose an option:
+                    Enter [1] To Update options in Inventory
+                    Enter [2] to View options in Inventory
+                    Enter [0] to EXIT""");
+                choice2 = input.nextInt();
+             break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + choice1);
+        }
+      switch (choice2){
+          case 1 :
+              var UPDATE = new UpdateInventory();
+              UPDATE.disp();
+              break;
+          case 2 : var VIEW = new ViewInventory();
+                VIEW.disp();
+          break;
+      }
+
+    }
+}
---
+++ admin/Gateway.java
@@ -0,0 +1,103 @@
+package admin;
+
+import login.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Gateway extends UpdateInventory{
+    private String Id;
+    private String Name;
+    private String City;
+    private String Type;
+    private String Price;
+    private String Number;
+    private String table;
+    public final int Choice;
+    Scanner input = new Scanner(System.in);
+
+    public Gateway(int ch,String table)  {
+        Choice =ch;
+        if(ch==1){
+           this.table= table;
+            try {
+                AddNew();
+                System.out.println("You Have been processed Back to Main menu");
+                disp();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+       if(ch==2){
+           this.table=table;
+           try {
+               delete();
+               System.out.println("You Have been processed Back to Main menu");
+               disp();
+           } catch (ClassNotFoundException | SQLException e) {
+               e.printStackTrace();
+           }
+       }
+    }
+
+    protected String queries(int ch){
+      return "";
+    }
+    private void getDetails(int ch){
+        switch(ch){
+            case 1 :System.out.print("Enter ID : ");
+                Id = input.nextLine();
+                System.out.print("Enter Name : ");
+                Name = input.nextLine();
+                System.out.print("Enter City : ");
+                City = input.nextLine();
+                System.out.print("Enter Type : ");
+                Type = input.nextLine();
+                System.out.print("Enter Price : ");
+                Price = input.nextLine();
+                System.out.print("Enter Number of persons : ");
+                Number = input.nextLine();
+                break;
+            case 2 : System.out.println("Delete Existing "+table+"entries only by Id");
+                System.out.println("Enter ID : ");
+                Id = input.nextLine();
+                break;
+
+        }
+    }
+
+
+
+
+    protected void AddNew() throws ClassNotFoundException, SQLException {
+        Connection connection = DBConnection.connect();
+        getDetails(Choice);
+        String sql = "insert into " + table + " (ID,Name,City,Type,Price,Number) values (?,?,?,?,?,?)";
+        PreparedStatement myStmt = connection.prepareStatement(sql);
+        myStmt.setString(1, Id);
+        myStmt.setString(2, Name);
+        myStmt.setString(3, City);
+        myStmt.setString(4, Type);
+        myStmt.setString(5, Price);
+        myStmt.setString(6, Number);
+        myStmt.executeUpdate();
+        System.out.println("Successfully registered");
+    }
+    protected void delete() throws ClassNotFoundException, SQLException {
+        Connection connection = DBConnection.connect();
+        getDetails(Choice);
+        String sql = "DELETE FROM "+table+" WHERE ID= ?";
+        PreparedStatement myStmt = connection.prepareStatement(sql);
+        myStmt.setInt(1, Integer.parseInt(Id));
+        myStmt.executeUpdate();
+        System.out.println("Successfully deleted");
+    }
+    protected void Alter() throws ClassNotFoundException {
+        Connection connection = DBConnection.connect();
+    }
+
+
+}
