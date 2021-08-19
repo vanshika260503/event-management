64e257b33c09c275804814f0c648559a190328a0
vanshika260503 <84037129+vanshika260503@users.noreply.github.com>
2021-08-19 16:42:24 +0530

    Delete customer directory

--- customer/BookingInterface.java
+++
@@ -1,8 +0,0 @@
-package customer;
-
-import java.sql.SQLException;
-
-public interface BookingInterface {
-	public  void Book() throws Exception;
-	public double getPrice() throws ClassNotFoundException, SQLException;
-}
--- customer/Payment.java
+++
@@ -1,8 +0,0 @@
-package customer;
-
-public class Payment {
-	public static void pay() {
-		System.out.println("      ****Paymment Option****     ");
-		System.out.println("");
-	}
-}
